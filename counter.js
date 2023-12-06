76715c75c001ddbef9668a96a05b17f0e1786b84
Rahelq <belayrahel500@gmail.com>
2023-12-06 19:16:30 +0300

    calculator and counter

---
+++ counter.js
@@ -0,0 +1,11 @@
+var count = 0;
+
+function increment() {
+  count++;
+  document.getElementById("count-display").textContent = count;
+}
+
+function decrement() {
+  count--;
+  document.getElementById("count-display").textContent = count;
+}
--- rachel.js
+++
@@ -1,8 +0,0 @@
-const mass= prompt("what is your mass")
-const height= prompt("what is your height")
-const bmi=mass/(height*height);
-
-console.log(bmi)
-
-
-
